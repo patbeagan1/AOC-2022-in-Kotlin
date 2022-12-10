403f9538e15e69edde0d9e1a088d34de0b6ccc47
Patrick Beagan <pbeagan1@gmail.com>
2022-12-10 10:34:50 -0600

    Getting column count working correctly

--- src/main/kotlin/dev/patbeagan/days/Day05.kt
+++ src/main/kotlin/dev/patbeagan/days/Day05.kt
@@ -10,8 +10,12 @@ class Day05 : AdventDay<String, String> {
     override fun part1(input: String): String {
         val (crateYard, moveCommands) = parseInput(input)
         moveCommands.forEach {
-            for (i in 0 until it.amount) {
-                crateYard.stacks[it.to - 1].push(crateYard.stacks[it.from - 1].pop())
+            try {
+                for (i in 0 until it.amount) {
+                    crateYard.stacks[it.to - 1].push(crateYard.stacks[it.from - 1].pop())
+                }
+            }catch (e: Exception) {
+                println()
             }
         }
         return crateYard.stacks.map { it.peek() }.joinToString("")
@@ -49,31 +53,40 @@ class Day05 : AdventDay<String, String> {
     value class CrateYard(
         val stacks: List<Stack<String>>,
     ) {
+        override fun toString(): String = stacks.joinToString("\n") {
+            it.toString()
+        }
+
         companion object {
             fun from(input: String): CrateYard {
                 val lines = input.split("\n")
                 val crateLines = lines.dropLast(1)
-                val numberOfColumns = (lines.last().length / 3)
+                val numberOfColumns = getColumns(lines)
                 val crateYard = CrateYard((0 until numberOfColumns).map { Stack<String>() })
-
-                crateLines
+                val map: List<List<Char>> = crateLines
                     .asReversed()
-                    .forEach { line ->
-                        debugShow(line)
-                        Regex("   |\\[([A-Z])\\]").findAll(line)
-                            .map { it.groupValues[1] }
-                            .forEachIndexed { index, s ->
-                                if (s.isNotBlank()) {
-                                    crateYard.stacks[index].push(s)
-                                }
-                            }
+                    .onEach { it.also { println(it) } }
+                    .map { s ->
+                        s
+                            .chunked(4)
+                            .map { it[1] }
                     }
+                map.forEach { each ->
+                    each.forEachIndexed { index, it ->
+                        if (it in CharRange('A', 'Z')) {
+                            crateYard.stacks[index].push(it.toString())
+                        }
+                    }
+                }
+                println(crateYard)
                 return crateYard
             }
 
-            private fun debugShow(line: String) {
-                line.replace(" [", "[").also { println(it) }
-            }
+            private fun getColumns(lines: List<String>): Int = lines
+                .last()
+                // todo calculate this directly instead of chunking - too memory intensive
+                .chunked(4)
+                .size
         }
     }
 }
