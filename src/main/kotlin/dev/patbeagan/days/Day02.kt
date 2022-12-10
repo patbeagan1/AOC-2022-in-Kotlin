554908ec32500123849b449a5797c77f88cc7df9
Patrick Beagan <pbeagan1@gmail.com>
2022-12-09 20:48:30 -0600

    adding in the solutions for day 2

--- src/main/kotlin/dev/patbeagan/days/Day02.kt
+++ src/main/kotlin/dev/patbeagan/days/Day02.kt
@@ -2,26 +2,27 @@ package dev.patbeagan.days
 
 import dev.patbeagan.AdventDay
 import dev.patbeagan.days.Day02.Move.*
-import dev.patbeagan.days.Day02.Outcome.*
 
 /**
  * [Day 2](https://adventofcode.com/2022/day/2)
  */
 class Day02 : AdventDay<String, Int> {
-    override fun part1(input: String) =
-        parseInput(input)
-            .fold(0) { acc, each ->
-                acc + Outcome.fromRound(each).points + each.self.points
-            }
-
-    override fun part2(input: String): Int = parseInput(input)
+    override fun part1(input: String) = parseInput1(input)
         .fold(0) { acc, each ->
-            acc + Outcome.fromRound(each).points + each.self.points
+            val movePoints = each.self.points
+            val outcomePoints = Outcome.fromRound(each).points
+            acc + outcomePoints + movePoints
         }
 
-    fun parseInput(input: String): List<Round> = input
-        .trim()
-        .split("\n")
+    override fun part2(input: String): Int = parseInput2(input)
+        .fold(0) { acc, each ->
+            val ensuringMoveFrom = each.second.getEnsuringMoveFrom(each.first)
+            val movePoints = ensuringMoveFrom.points
+            val outcomePoints = each.second.points
+            acc + movePoints + outcomePoints
+        }
+
+    fun parseInput1(input: String): List<Round> = getLines(input)
         .map { s ->
             s.split(
                 " "
@@ -32,15 +33,34 @@ class Day02 : AdventDay<String, Int> {
             }
         }
 
+    fun parseInput2(input: String): List<Pair<Move, Outcome>> = getLines(input)
+        .map { s ->
+            s.split(
+                " "
+            ).let {
+                Move.from(it[0])!! to Outcome.from(it[1])!!
+            }
+        }
+
+    private fun getLines(input: String) = input
+        .trim()
+        .split("\n")
+
+    /**
+     * A single round of rock-paper-scissors.
+     */
     data class Round(val self: Move, val opponent: Move)
 
+    /**
+     * A single move.
+     */
     enum class Move(
         val points: Int,
     ) {
         Rock(1),
         Paper(2),
         Scissors(3);
-        
+
         companion object {
             private val byToken = mapOf(
                 "A" to Rock,
@@ -55,12 +75,33 @@ class Day02 : AdventDay<String, Int> {
         }
     }
 
+    /**
+     * The outcome of a round.
+     */
     enum class Outcome(val points: Int) {
         Win(6),
         Tie(3),
         Loss(0);
 
+        /**
+         * Given an end state and the opponent's move,
+         * this returns the move that you need to do to get to that end state
+         */
+        fun getEnsuringMoveFrom(move: Move): Move = when (this) {
+            Win -> winStates.first { it.opponent == move }.self
+            Tie -> move
+            Loss -> winStates.first { it.self == move }.opponent
+        }
+
         companion object {
+            private val byToken = mapOf(
+                "X" to Loss,
+                "Y" to Tie,
+                "Z" to Win,
+            )
+
+            fun from(input: String): Outcome? = byToken[input]
+
             fun fromRound(round: Round) = when {
                 round.self == round.opponent -> Tie
                 winStates.contains(round) -> Win
--- src/test/kotlin/dev/patbeagan/days/Day02Test.kt
+++ src/test/kotlin/dev/patbeagan/days/Day02Test.kt
@@ -1,7 +1,6 @@
 package dev.patbeagan.days
 
-import dev.patbeagan.days.Day02.Move
-import dev.patbeagan.days.Day02.Round
+import dev.patbeagan.days.Day02.*
 import dev.patbeagan.obtainFile
 import org.junit.Test
 import org.testng.Assert
@@ -10,13 +9,28 @@ class Day02Test {
     private val sub = Day02()
 
     @Test
-    fun `parsing works correctly`() {
+    fun `parsing works correctly - part 1`() {
         val expected = listOf(
             Round(Move.Paper, Move.Rock),
             Round(Move.Rock, Move.Paper),
             Round(Move.Scissors, Move.Scissors),
         )
-        val actual: List<Round> = sub.parseInput(
+        val actual: List<Round> = sub.parseInput1(
+            "Day02_input.txt"
+                .obtainFile()
+                .readText()
+        )
+        Assert.assertEquals(actual, expected)
+    }
+
+    @Test
+    fun `parsing works correctly - part 2`() {
+        val expected = listOf(
+            Move.Rock to Outcome.Tie,
+            Move.Paper to Outcome.Loss,
+            Move.Scissors to Outcome.Win,
+        )
+        val actual: List<Pair<Move, Outcome>> = sub.parseInput2(
             "Day02_input.txt"
                 .obtainFile()
                 .readText()
@@ -54,6 +68,17 @@ class Day02Test {
                 .readText()
         )
         val expected = 12
+        Assert.assertEquals(actual, expected)
+    }
+
+    @Test
+    fun `part 2 answer`() {
+        val actual = sub.part2(
+            "Day02_data.txt"
+                .obtainFile()
+                .readText()
+        )
+        val expected = 11998
         Assert.assertEquals(actual, expected)
     }
 }
