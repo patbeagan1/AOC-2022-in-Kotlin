15c00464b6f2c67965633115f9067a71fd43a370
Patrick Beagan <pbeagan1@gmail.com>
2022-12-11 12:59:40 -0600

    Improving the 6.2 implementation

--- src/main/kotlin/dev/patbeagan/days/Day06.kt
+++ src/main/kotlin/dev/patbeagan/days/Day06.kt
@@ -21,16 +21,12 @@ class Day06 : AdventDay<Int> {
     }
 
     override fun part2(input: String): Int {
-        val buffer = mutableListOf<Char>()
-        input.forEachIndexed { index, each ->
-            buffer.add(each)
-            if (buffer.size == 14) {
-                if (buffer.distinct().size == buffer.size) {
-                    return index + 1
-                }
-                buffer.removeAt(0)
+        val offset = 14
+        input
+            .windowedSequence(offset)
+            .forEachIndexed { index, each ->
+                if (each.toSet().size == each.length) return index + offset
             }
-        }
         return -1
     }
 
